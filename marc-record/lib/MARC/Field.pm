a03ef3b9bc1a7c33ae86716b5f34d08d30590994
moregan <moregan>
2003-04-04 17:01:24 +0000

    more docs for as_string()

--- marc-record/lib/MARC/Field.pm
+++ marc-record/lib/MARC/Field.pm
@@ -353,12 +353,26 @@ sub add_subfields(@) {
 
 =head2 as_string( [$subfields] )
 
-Returns a string of all subfields run together, without the tag number.
+Returns a string of all subfields run together.  A space is added to
+the result between each subfield.  The tag number and subfield 
+character are not included.
+
+Subfields appear in the output string in the order in which they
+occur in the field. 
 
 If C<$subfields> is specified, then only those subfields will be included.
 
-    my $field = $marc->field( '245' );
-    print $field->as_string( 'anp' ); # Only those three subfields
+  my $field = MARC::Field->new( 
+		245, '1', '0',
+			'a' => 'Abraham Lincoln',
+			'h' => '[videorecording] :',
+			'b' => 'preserving the union /',
+			'c' => 'A&E Home Video.'
+		);
+  print $field->as_string( 'abh' ); # Only those three subfields
+  # prints 'Abraham Lincoln [videorecording] : preserving the union /'.
+
+Note that subfield h comes before subfield b in the output.
 
 =cut
 
