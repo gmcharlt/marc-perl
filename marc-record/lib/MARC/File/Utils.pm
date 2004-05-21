385de73714c5dc1264edd189131fdfcc3359628a
edsummers <edsummers>
2004-05-21 01:41:34 +0000

    added comment, removed redundant POD

--- marc-record/lib/MARC/File/Utils.pm
+++ marc-record/lib/MARC/File/Utils.pm
@@ -1,5 +1,10 @@
 package MARC::File::Utils;
 
+# these are utility functions that need to live in a separate namespace
+# from MARC::File::USMARC since they use the Encode module, which exports
+# encode() and decode() by default, and interfere with functions of the
+# same name in MARC::File::USMARC and friends.
+
 use strict;
 
 sub safe_substr {
@@ -24,34 +29,3 @@ sub utf8_safe {
 }
 
 1;
-
-__END__
-
-=head1 RELATED MODULES
-
-L<MARC::Record>
-
-=head1 TODO
-
-Make some sort of autodispatch so that you don't have to explicitly
-specify the MARC::File::X subclass, sort of like how DBI knows to
-use DBD::Oracle or DBD::Mysql.
-
-Create a toggle-able option to check inside the field data for
-end of field characters.  Presumably it would be good to have
-it turned on all the time, but it's nice to be able to opt out
-if you don't want to take the performance hit.
-
-=head1 LICENSE
-
-This code may be distributed under the same terms as Perl itself.
-
-Please note that these modules are not products of or supported by the
-employers of the various contributors to the code.
-
-=head1 AUTHOR
-
-Andy Lester, C<< <andy@petdance.com> >>
-
-=cut
-
