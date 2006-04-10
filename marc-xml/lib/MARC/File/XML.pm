7db9d1ceed57b1b06f7d7fbc368f94f7ae7ab2ef
miker-pines <miker-pines>
2006-04-10 18:46:20 +0000

    fixing quote bug spotted by Tumer Garip

--- marc-xml/lib/MARC/File/XML.pm
+++ marc-xml/lib/MARC/File/XML.pm
@@ -334,7 +334,7 @@ sub record {
 <record
   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
   xsi:schemaLocation="http://www.loc.gov/MARC21/slim http://www.loc.gov/ standards/marcxml/schema/MARC21slim.xsd"
-  xmlns="http://www.loc.gov/MARC21/slim>
+  xmlns="http://www.loc.gov/MARC21/slim">
 HEADER
 
     } else {
