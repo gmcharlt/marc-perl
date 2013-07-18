e3a1e9bfcf3c25bf4113fa5d23f7f11c5a6bd7f9
Galen Charlton <gmcharlt@gmail.com>
2013-07-18 16:11:21 -0700

    prepare for 0.03 MARC-MiJ release
    Signed-off-by: Galen Charlton <gmcharlt@gmail.com>

--- marc-mij/lib/MARC/File/MiJ.pm
+++ marc-mij/lib/MARC/File/MiJ.pm
@@ -13,11 +13,11 @@ MARC::File::MiJ - Read newline-delimited marc-in-json files
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
--- marc-mij/lib/MARC/Record/MiJ.pm
+++ marc-mij/lib/MARC/Record/MiJ.pm
@@ -11,11 +11,11 @@ MARC::Record::MiJ - Convert MARC::Record to/from marc-in-json structure
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
