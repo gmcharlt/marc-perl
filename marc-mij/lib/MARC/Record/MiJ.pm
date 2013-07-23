e23de57d8101281bbae2d0309e2bc893b998b15b
Galen Charlton <gmcharlt@gmail.com>
2013-07-23 10:26:04 -0700

    prepare for 0.04 release of MARC-MiJ
    Signed-off-by: Galen Charlton <gmcharlt@gmail.com>

--- marc-mij/lib/MARC/Record/MiJ.pm
+++ marc-mij/lib/MARC/Record/MiJ.pm
@@ -11,11 +11,11 @@ MARC::Record::MiJ - Convert MARC::Record to/from marc-in-json structure
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
